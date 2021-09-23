402d547288e6ba845fc4669192bbd16c29b9d8f2
DiegoO9 <ortegafran909@gmail.com>
2021-09-23 13:37:53 -0500

    Borrador

--- js/form.js
+++ js/form.js
@@ -1,34 +1,47 @@
 
+var valor = document.getElementById("nombre");
+function checkNombre (valor) {
+  
 
-function checkNombre (valor){
-    var nombre = document.getElementsById("nombre");
-    var letters = /^[A-Za-z]+$/;   
-    console.log("enviando...");
-    
-    if(form.nombre.value.match(letters))
-      {
-        if (nombre.value !== null || form.nombre.value  !== "")
-        {
-            if(nombre.value.length>=4&& form.nombre.value.length <=30)
-            {
-              return true;
-            }
-            else
-            {                
-              return false;
-            }  
-        }
-        else
-        {
-          return false;
-        }
-      
-      }
-    else
-      {        
-        return false;
-      }
-
+  function estaVacio(cadena) {
+  if(cadena.length=0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-module.exports= checkNombre;
+function validarLongitudCadena(cadena, minimo, maximo) {
+  if(cadena.length >= minimo && cadena.length <=maximo) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function NoContieneNumeros(cadena) {  
+  const expresion1 = /^[A-Za-z]+$/;   
+  if(cadena.match(expresion1)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+  if(estaVacio(valor)) {
+    //alert("El nombre no puede estar vacío");
+    return false;
+  } else if(!validarLongitudCadena(valor, 4, 30)) {
+    //alert("El nombre debe tener una longitud entre 4 y 30")
+    return false;
+  } else if(!NoContieneNumeros(valor)) {
+    //alert("El nombre no puede contener números");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+//module.exports= checkNombre;
+
+
