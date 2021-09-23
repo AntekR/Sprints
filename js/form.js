cd6600b34cd49e2459768f036b784ac9cbcfdd32
AntekR <83246281+AntekR@users.noreply.github.com>
2021-09-23 13:37:07 -0500

    Avance

--- js/form.js
+++ js/form.js
@@ -1,1 +1,40 @@
+// Variables
+const form = document.getElementById('form-registro');
+// Selecciona todo lo que halla en los campos
+const direction = document.getElementById('direccion');
+const inputs = document.querySelectorAll('#direccion');
 
+
+
+
+function checkDir(direction){
+    // Expresion verificadora
+    
+    if(direction!=null){
+        var check = true;
+        const character = /^[a-zA-Z0-9#-]{0,50}$/;
+        // Verifica los campos
+        function field (){
+            valid(character, direction);
+        }
+        // Verifica los caracteres
+        function valid(expresion,input){
+            if(expresion.test(input.value)){
+                check = true;
+                console.log('Caracter valido'); 
+                // document.getElementById(`#${campo}`).classList.remove('direction_failed');
+                // document.getElementById(`#${campo}`).classList.remove('direction_correct');
+            }else{
+                check = false;
+                console.log('Caracter invalido');
+            }
+        }
+        // Verifica cada tecla presionada
+        inputs.forEach((input) => {
+            input.addEventListener('keyup', field);
+        });
+    }
+    return check;
+}
+
+checkDir();
