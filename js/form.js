b44700fddf2bf595d10c8c02c2e576820ac3b245
DiegoO9 <ortegafran909@gmail.com>
2021-09-24 16:30:17 -0500

    borrador4

--- js/form.js
+++ js/form.js
@@ -5,14 +5,14 @@ function checkNombre (valor) {
     return false;
   } else if(valor.length < 4 || valor.length > 30) {
     return false;
-  } else if(valor.indexOf("0") >= 0 || valor.indexOf("1") >= 0 || 
-  valor.indexOf("2") >= 0 || valor.indexOf("3") >= 0 || valor.indexOf("4") >= 0 
-  || valor.indexOf("5") >= 0 || valor.indexOf("6") >= 0 || valor.indexOf("7") >= 0 
-  || valor.indexOf("8") >= 0 || valor.indexOf("9") >= 0) {
+  } else if(valor.includes("0")|| valor.includes("1") || 
+  valor.includes("2") || valor.includes("3") || valor.includes("4") 
+  || valor.includes("5") || valor.includes("6") || valor.includes("7")  
+  || valor.includes("8") || valor.includes("9") ) {
     return false;
   } else {
     return true;
   }
 }
 
-module.exports = checkNombre;
+//module.exports = checkNombre;
