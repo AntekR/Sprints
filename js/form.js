a2e1c4e04a167e63d71483a3b659aed386403355
PuerKso <juanj.martinez.1007@gmail.com>
2021-09-24 16:19:08 -0500

    form telefono

--- js/form.js
+++ js/form.js
@@ -1,1 +1,18 @@
+function checkTelefono (valor) {
+  const enteros = /^(?:\+|-)?\d+$/;
+  if(valor.length==0) {
+    return false;
+  } else if(valor.length < 7 || valor.length > 7) {
+    return false;
+  } else if(valor.indexOf("0") !=enteros || valor.indexOf("1") !=enteros || 
+  valor.indexOf("2") !=enteros || valor.indexOf("3") !=enteros || valor.indexOf("4") !=enteros 
+  || valor.indexOf("5") !=enteros || valor.indexOf("6") !=enteros){
+    return false;
+  } else if(valor = 7){
+    return true;
+  } else {
+    return true;
+  }
+}
 
+module.exports = checkTelefono;
